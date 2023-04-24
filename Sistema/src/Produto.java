a76faa360b3fa2f6996faae70ea272dd760990e3
Felipe <felipefggw@gmail.com>
2023-04-24 20:43:27 -0300

    alteracao

--- Sistema/src/Produto.java
+++ Sistema/src/Produto.java
@@ -1,3 +1,12 @@
+/*
+ * Classe Produto: Crie uma classe chamada "Produto" com os atributos: id (int), nome
+(String), descricao (String) e preco (double). Adicione métodos para definir (set) e
+obter (get) os valores desses atributos e um método para exibir as informações
+completas do produto dentro de uma String
+ * 
+ */
+
+
 
 public class Produto {
 	
