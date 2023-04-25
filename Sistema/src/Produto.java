a7aa007c78cf284302f07658dac19d8f4dd88533
Felipe Teixeira <felipefggw@gmail.com>
2023-04-25 11:43:56 -0300

    Teste

--- Sistema/src/Produto.java
+++ Sistema/src/Produto.java
@@ -1,12 +1,7 @@
-/*
- * Classe Produto: Crie uma classe chamada "Produto" com os atributos: id (int), nome
+/*Classe Produto: Crie uma classe chamada "Produto" com os atributos: id (int), nome
 (String), descricao (String) e preco (double). Adicione métodos para definir (set) e
 obter (get) os valores desses atributos e um método para exibir as informações
-completas do produto dentro de uma String
- * 
- */
-
-
+completas do produto dentro de uma String.*/
 
 public class Produto {
 	
